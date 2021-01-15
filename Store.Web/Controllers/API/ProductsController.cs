8b70f351b9d8d89570309145dc0c054266f51873
Tiago Pires <Tiago.Goncalves.Pires@formandos.cinel.pt>
2021-01-15 15:40:55 +0000

    Add and Edit Images

---
+++ Store.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace Store.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Store.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
---
+++ Store.Web/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+    using Store.Web.Data.Entities;
+    
+
+    [Display(Name = "Image")]
+    public class ProductViewModel : Product
+    {
+        public IFormFile ImageFile { get; set; }
+    }
+
+}
