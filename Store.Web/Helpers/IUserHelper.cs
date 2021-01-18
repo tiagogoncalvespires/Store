60d7c2f15845434d7a5c632cee115f3dbb648ef0
Tiago Pires <Tiago.Goncalves.Pires@formandos.cinel.pt>
2021-01-18 18:31:35 +0000

    Web Change user and password.

--- Store.Web/Helpers/IUserHelper.cs
+++ Store.Web/Helpers/IUserHelper.cs
@@ -19,5 +19,10 @@ namespace Store.Web.Helpers
 
 
         Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
     }
 }
